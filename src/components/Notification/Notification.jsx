8cee25f515a1a6b050bef3e46801f6fbb6b33d57
GOOSxE <der.goose.4308@gmail.com>
2023-07-09 23:59:31 +0200

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import propTypes from 'prop-types';
 // ? // Компонент нотифікацій якщо відгуків ще немає ;
 const Notification = ({ children }) => <h4>{children}</h4>;
 export default Notification;
